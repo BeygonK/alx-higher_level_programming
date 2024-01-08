af03c9fabb7704a23da39b2ce34d3ac5daadc1c1
BeygonK <kipkoechbeygon@gmail.com>
2024-01-08 14:00:20 -0800

    alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-import string; __import__('sys').stdout.buffer.write(bytes(string.ascii_uppercase + '\n', 'utf-8'))
+import string; print(*list(string.ascii_uppercase), sep='', end='\n')
