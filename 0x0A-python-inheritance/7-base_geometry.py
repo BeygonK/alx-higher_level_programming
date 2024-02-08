a36fc535b7698bab4e1534260db003a3bd4188f1
BeygonK <kipkoechbeygon@gmail.com>
2024-02-08 00:52:06 -0800

    Tests

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,6 +24,6 @@ class BaseGeometry:
                 ValueError: if value is < 0
         """
         if not isinstance(value, int):
-            raise TypeError('{} must be an integer'.format(self.name))
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(self.name))
+            raise ValueError('{} must be greater than 0'.format(name))
