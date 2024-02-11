4e29bbd758553e9d5f6173d9913b0cd9a128b56c
BeygonK <kipkoechbeygon@gmail.com>
2024-02-10 22:18:51 -0800

    add load save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a function"""
+
+
+import json
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+arguments = sys.argv[1:]
+filename = "add_item.json"
+save_to_json_file(arguments, filename)
+existing_list = load_from_json_file(filename)
+existing_list.extend(arguments)
+save_to_json_file(existing_list, filename)
