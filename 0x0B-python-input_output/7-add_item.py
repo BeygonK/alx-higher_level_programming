975a313768735eeb79efa6b354e757df66f2dbaa
BeygonK <kipkoechbeygon@gmail.com>
2024-02-10 22:27:46 -0800

    document

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
+"""Get the command line args"""
 arguments = sys.argv[1:]
 filename = "add_item.json"
+
+"""Save the list to a file"""
 existing_list = load_from_json_file(filename)
 existing_list.extend(arguments)
 save_to_json_file(existing_list, filename)
