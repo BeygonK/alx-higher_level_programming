dffc0f0bd54a84d873cd07ad230723cbdb13a98f
BeygonK <kipkoechbeygon@gmail.com>
2024-02-14 00:11:20 -0800

    fixed error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -56,7 +56,9 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if value < 0:
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        elif value < 0:
             raise ValueError('x must be >= 0')
         else:
             self.__x = value
@@ -67,6 +69,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
         if value < 0:
             raise ValueError('y must be >= 0')
         else:
