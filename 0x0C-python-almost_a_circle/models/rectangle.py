d3e66666f50168e8856121ef1abcbc1c9d9f1417
BeygonK <kipkoechbeygon@gmail.com>
2024-02-14 20:34:11 -0800

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,10 +83,10 @@ class Rectangle(Base):
 
     def display(self):
         """This method displays rectangle"""
-        for _ in range(self.__height):
-            for _ in range(self.__width):
-                print("#", end="")
+        for _ in range(self.__y):
             print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
