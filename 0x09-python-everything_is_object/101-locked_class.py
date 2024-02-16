f3edd871398f3f26975cb4c93843cb236740a155
BeygonK <kipkoechbeygon@gmail.com>
2024-02-16 11:15:29 -0800

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""This module is a class"""
+
+
 class LockedClass:
+    """This is a class module"""
     def __setattr__(self, name, value):
         if name != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
