9fa44406ff28e82da0d0cfcb384a6825915eef91
BeygonK <kipkoechbeygon@gmail.com>
2024-02-16 11:31:40 -0800

    add attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
 
 
 def add_new_attribute(obj, attr_name, attr_value):
+    """This function adds new attribute if possible"""
     if hasattr(obj, '__dict__') or (hasattr(type(obj), '__slots__') and attr_name in type(obj).__slots__):
         setattr(obj, attr_name, attr_value)
     else:
