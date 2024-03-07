4d88c126fb712117e6ea387c01f89611d99f9809
BeygonK <kipkoechbeygon@gmail.com>
2024-03-06 22:44:59 -0800

    update to lwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,11 +91,16 @@ class Rectangle(Base):
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """This method updates the values"""
-        if len(args) > 0:
+        if args:
             self.id = args[0] if len(args) >= 1 else self.id
             self.__width = args[1] if len(args) >= 2 else self.__width
             self.__height = args[2] if len(args) >= 3 else self.__height
             self.__x = args[3] if len(args) >= 4 else self.__x
             self.__y = args[4] if len(args) >= 5 else self.__y
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+                
