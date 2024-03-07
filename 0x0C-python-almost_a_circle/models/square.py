44d966fa418a4957e52cb4b0eae7571f402208ad
BeygonK <kipkoechbeygon@gmail.com>
2024-03-06 23:06:34 -0800

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+"""This is a square class that inherits Rectangle"""
+
+from .rectangle import Rectangle
+
+class Square(Rectangle):
+    """This is a class initialization"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """This is a constructor"""
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width
+                if hasattr(self, 'width') else self.height)
+        
