d000de9bd0b0e77046fc4cdec9c5768f8ed4462f
BeygonK <kipkoechbeygon@gmail.com>
2024-03-06 23:11:03 -0800

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
     
     def __str__(self):
+        """Prints string"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width
                 if hasattr(self, 'width') else self.height)
         
