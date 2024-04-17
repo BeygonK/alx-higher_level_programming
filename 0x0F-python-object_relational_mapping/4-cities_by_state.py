3b7246c4616ae817a1022806693f23285e006548
BeygonK <kipkoechbeygone@gmail.com>
2024-04-17 09:08:37 -0700

    get cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""List cities"""
+
+import MySQLdb
+from sys import argv
+
+
+def get_cities(username, password, database):
+    """Function to get cities"""
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            db=database)
+    cursor = db.cursor()
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    get_cities(username, password, database)
