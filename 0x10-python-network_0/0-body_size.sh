41db410285b3b1ec10b674b5dc504831fbe6c5d6
BeygonK <kipkoechbeygone@gmail.com>
2024-04-28 13:05:58 +0300

    display size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends request and displays
-curl -sI "$url" | grep -i '^Content-Length:' | awk '{print $2}'
+curl -sI "$url" | grep -i Content-Length | cut -d " " -f 2 
