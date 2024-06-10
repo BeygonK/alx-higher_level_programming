c72d95db0c4ccdab9a3a4ca9fbd82207283fb2c8
BeygonK <kipkoechbeygone@gmail.com>
2024-06-10 11:25:58 +0300

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create a new user if doesn't exist and grant all privileges --
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
